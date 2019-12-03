834972eb8629f50c2e7d5dce72655b15839db55a
liamwb <liam.woodbaker@gmail.com>
2019-12-03 14:38:54 +1100

    This stuff is pretty confusing

--- SportsBet.py
+++ SportsBet.py
@@ -2,6 +2,7 @@
 
 # I have started by following this tutorial: https://www.edureka.co/blog/web-scraping-with-python/
 
+import re # it's regex
 from selenium import webdriver  # selenium is for accessing webpages
 import bs4  # BeautifulSoup is for data from websites
 import pandas  # pandas is for data analysis
@@ -13,3 +14,5 @@ url = input("input Sportsbet url: ")
 driver.get(url)
 content = driver.page_source
 soup = bs4.BeautifulSoup(content, features="html.parser")  # soup now contains the html behind url
+# for i in re.findall("li", soup.body.prettify()):#soup.find_all("li") is a list of all the <li> tags in the soup
+
