6b06144446de4b76aca99f3032ee5115a671175b
liamwb <liam.woodbaker@gmail.com>
2019-12-04 13:11:37 +1100

    the fillArrays() function now scrapes all the data from sportsbet into the relevant arrays.
    the convertOddsArraysToFloats() function does what it says on the tin, and catches errors thrown up when betting is suspended on a game, setting the odds to 0.00.

--- SportsBet.py
+++ SportsBet.py
@@ -14,17 +14,40 @@ url = input("input Sportsbet url: ")
 driver.get(url)
 content = driver.page_source
 soup = bs4.BeautifulSoup(content, features="html.parser")  # soup now contains the html behind url
-for i in soup.select('ul > div > ul > li'): # the tags containing the data for each game are in this structure
-    i = str(i)
 
-    # find teamA
-    teamAStartIndex = i.find("event-participant-1") + 21 # 21 because "event-participant-1" is 19 characters, and then there's a " and a >
-    teamAEndIndex = i[teamAStartIndex:].find(r"<span") + teamAStartIndex -1
-    teamA.append(i[teamAStartIndex:teamAEndIndex])
 
-    # find teamB
+def fillArrays():
+    for i in soup.select('ul > div > ul > li'):  # the tags containing the data for each game are in this structure
+        i = str(i)
 
-    # find oddsA
+        # find teamA
+        teamAStartIndex = i.find("event-participant-1") + 21
+        # 21 because "event-participant-1" is 19 characters, and then there's a " and a >
+        teamAEndIndex = i[teamAStartIndex:].find("</span") + teamAStartIndex
+        teamA.append(i[teamAStartIndex:teamAEndIndex])
 
-    # find oddsB
+        # find oddsA
+        # the odds are after ' "price-text">
+        # and are always three digits long (x.xx so four characters)
+        oddsAStartIndex = i[teamAStartIndex:].find(
+            "price-text") + teamAStartIndex + 12  # "price-text" is 10 chars, plus the ">
+        oddsA.append(i[oddsAStartIndex:oddsAStartIndex + 4])
 
+        # find teamB
+        teamBStartIndex = i.find("event-participant-2") + 21
+        teamBEndIndex = i[teamBStartIndex:].find("</span") + teamBStartIndex
+        teamB.append(i[teamBStartIndex:teamBEndIndex])
+
+        # find oddsB
+        oddsBStartIndex = i[oddsAStartIndex:].find("price-text") + oddsAStartIndex + 12
+        oddsB.append(i[oddsBStartIndex: oddsBStartIndex + 4])
+
+
+def convertOddsArrayToFloats(array):
+    for i in array:
+        try:
+            i = float(i)
+        except ValueError:
+            i = 0.00
+            print("Betting has been suspended for one game")
+        # If betting has been suspended then weird shit shows up where the odds usually are
