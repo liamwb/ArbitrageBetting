56e1be3c5768ffcc4714a2211af9bc4758b09048
Liam's Laptop <liam.woodbaker@gmail.com>
2019-12-21 20:45:36 +1100

    I am committing the modules that are used. I don't know if that is good practice or not...

--- NedsTennis.py
+++ NedsTennis.py
@@ -13,10 +13,12 @@ content = driver.page_source
 soup = bs4.BeautifulSoup(content, features="html.parser")  # soup now contains the html behind url
 
 def fillArrays():
-    teamAIndicies = []
-    teamBIndicies = []
-    oddsAInidicies = []
-    oddsBIndicies = []
+    pbnArray = soup.find_all(class_="price-button-name")
+    # every even index is the name of a teamA, and vice-versa for the odd indicies
+    for i in range(0, len(pbnArray)):
+        if i % 2 == 0:
+            teamA.append(i)
+        elif i % 2 == 1:
+            teamB.append(i)
 
-    soup.find_all()
-
+fillArrays()
